79a2a97e2408ddf2d8e47456090607dfc2f19647
Oluka Akin <30444669+olukaz@users.noreply.github.com>
2025-01-12 16:26:50 +0100

    test

--- inventories/views.py
+++ inventories/views.py
@@ -28,7 +28,7 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductMultiViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     parser_classes = [MultiPartParser]  # Allow file upload
