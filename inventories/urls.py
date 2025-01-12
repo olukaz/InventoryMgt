e279bdeb6d1e0ce5dc62be46372b145e55b86beb
Oluka Akin <30444669+olukaz@users.noreply.github.com>
2025-01-12 20:41:13 +0100

    test

--- inventories/urls.py
+++ inventories/urls.py
@@ -20,7 +20,7 @@ router = DefaultRouter()
 router.register(r'suppliers', SupplierViewSet)
 router.register(r'products', ProductViewSet)
 router.register(r'inventory', InventoryLevelViewSet)
-router.register(r'multiproducts', ProductMultiViewSet)
+# router.register(r'multiproducts', ProductMultiViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
