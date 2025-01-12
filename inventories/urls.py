94900f9f4f46bce1b658090917cb422525d8f54e
Oluka Akin <30444669+olukaz@users.noreply.github.com>
2025-01-12 21:34:43 +0100

    test

--- inventories/urls.py
+++ inventories/urls.py
@@ -17,10 +17,10 @@ from .views import *
 
 
 router = DefaultRouter()
+router.register(r'filehandling', FilehandlingViewSet)
 router.register(r'suppliers', SupplierViewSet)
 router.register(r'products', ProductViewSet)
 router.register(r'inventory', InventoryLevelViewSet)
-router.register(r'filehandling', FilehandlingViewSet)
 
 
 urlpatterns = [
