47d4eae0c202e62e4c1ee71265600f4a5ff386e7
Oluka Akin <30444669+olukaz@users.noreply.github.com>
2025-01-13 07:24:36 +0100

    again

--- InventoryMgt/settings.py
+++ InventoryMgt/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventories'
+    'inventories',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
